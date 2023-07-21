ac8c3f49cad46738a19a9778fde8e5ba05c5a75a
Mann <meruliyamann26100@gmail.com>
2023-07-21 13:33:47 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const homepage(),
+    );
+  }
+}
+
+class homepage extends StatefulWidget {
+  const homepage({super.key});
+
+  @override
+  State<homepage> createState() => _homepageState();
+}
+
+class _homepageState extends State<homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+
+        title: Text("Solar System"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Lottie.asset("assets/solarsystem.json",width: 300)
+      ),
+    );
+  }
+}
